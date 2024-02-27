5bc68af278d35465b325f3a8cceda502c1487973
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-02-27 09:01:30 +0200

    Header

--- source/js/script.js
+++ source/js/script.js
@@ -28,10 +28,11 @@ class App {
         if (navMain.classList.contains('main-nav--closed')) {
           navMain.classList.remove('main-nav--closed');
           navMain.classList.add('main-nav--opened');
-          navButtonText.classList.add('visually-hidden');
+          navButtonText.textContent = "Close"
         } else {
           navMain.classList.add('main-nav--closed');
           navMain.classList.remove('main-nav--opened');
+          navButtonText.textContent = "Меню"
         }
       });
     }
