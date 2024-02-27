2eb1c437cb87384085590a939478e13a987c8b9f
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-02-27 19:44:21 +0200

    Results

--- source/js/script.js
+++ source/js/script.js
@@ -77,6 +77,8 @@ class App {
             settings: {
               slidesToShow: 1,
               dots: true,
+              autoplay: true,
+              autoplaySpeed: 3000,
             },
           },
         ],
