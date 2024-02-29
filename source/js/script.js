ab0f7c3c59ec8cec217b77154f50d61d79b95332
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-02-29 17:19:22 +0200

    Delete less code + New font

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,10 @@
-// MENU
-
 class App {
   init() {
     this.initMobileMenu();
-    this.initSlider();
-    this.afterVideoPlay();
-    this.onButtonPlay();
-    this.reviewsVideoPlay()
     this.onFaqBtnClick();
   }
 
   constructor() {
-    this.iframe = document.querySelector("iframe");
-    this.player = new Vimeo.Player(this.iframe);
-    this.btnPlay = document.querySelector("#button-play");
-    this.promoVideoContainer = document.querySelector(
-      ".promo__video-container"
-    );
-    this.reviewsVideos = document.querySelectorAll(".review__video");
     this.faqBtns = document.querySelectorAll(".faq-btn");
   }
 
@@ -61,99 +48,6 @@ class App {
     initJS();
     closeOpen();
     linksClick();
-  }
-
-  initSlider() {
-    $(function () {
-      $(".slider").slick({
-        arrows: false,
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              dots: true,
-              autoplay: true,
-              autoplaySpeed: 3000,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              dots: true,
-              autoplay: true,
-              autoplaySpeed: 3000,
-            },
-          },
-        ],
-      });
-
-      $(".slider-about").slick({
-        slidesToShow: 1,
-        centerMode: true,
-        centerPadding: "380px",
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              centerMode: false,
-              autoplay: true,
-              autoplaySpeed: 3000,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              centerMode: false,
-              slidesToShow: 1,
-              autoplay: true,
-              autoplaySpeed: 3000,
-            },
-          },
-        ],
-      });
-    });
-  }
-
-  afterVideoPlay() {
-    const videoWrapper = document.querySelector(".promo__video");
-
-    const onPlay = () => {
-      videoWrapper.style.borderRadius = "unset";
-      this.btnPlay.style.display = "none";
-    };
-
-    this.player.on("play", onPlay);
-  }
-
-  onButtonPlay() {
-    const playVideo = () => {
-      this.player.play();
-      this.btnPlay.style.display = "none";
-      this.promoVideoContainer.classList.add("active");
-    };
-
-    this.btnPlay.addEventListener("click", playVideo);
-  }
-
-  reviewsVideoPlay() {
-    const handlePlay = (wrapper) =>{
-      const btnPlay = wrapper.querySelector('.btn-play')
-      const iframe = wrapper.querySelector("iframe");
-      const player = new Vimeo.Player(iframe);
-
-      const play = () => {
-        player.play()
-        btnPlay.style.display = "none";
-      }
-
-      btnPlay.addEventListener('click', play)
-    }
-
-    this.reviewsVideos.forEach((rv)=>handlePlay(rv))
   }
 
   onFaqBtnClick() {
