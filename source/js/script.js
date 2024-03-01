a5f8dd46ce77f3fc8414aaca398e7df26c237fad
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-03-01 14:47:50 +0200

    Mobile menu

--- source/js/script.js
+++ source/js/script.js
@@ -9,41 +9,40 @@ class App {
   }
 
   initMobileMenu() {
-    const navMain = document.querySelector(".main-nav");
-    const navToggle = document.querySelector(".main-nav__toggle");
-    const navButtonText = document.querySelector(".main-nav__open-btn-text");
+    const navMain = document.querySelector('.main-nav');
+    const navToggle = document.querySelector('.main-nav__toggle');
+    const navButtonText = document.querySelector('.main-nav__open-btn-text');
 
     const initJS = () => {
-      navMain.classList.remove("main-nav--nojs");
-    };
+      navMain.classList.remove('main-nav--nojs');
+    }
 
     const closeOpen = () => {
-      navToggle.addEventListener("click", function () {
-        if (navMain.classList.contains("main-nav--closed")) {
-          navMain.classList.remove("main-nav--closed");
-          navMain.classList.add("main-nav--opened");
-          navButtonText.textContent = "Close";
+      navToggle.addEventListener('click', function () {
+        if (navMain.classList.contains('main-nav--closed')) {
+          navMain.classList.remove('main-nav--closed');
+          navMain.classList.add('main-nav--opened');
+          navButtonText.classList.remove('visually-hidden');
         } else {
-          navMain.classList.add("main-nav--closed");
-          navMain.classList.remove("main-nav--opened");
-          navButtonText.textContent = "Меню";
+          navMain.classList.add('main-nav--closed');
+          navMain.classList.remove('main-nav--opened');
         }
       });
-    };
+    }
 
     const linksClick = () => {
-      const mainNav = document.querySelector(".main-nav");
-      const links = mainNav.querySelectorAll("a");
+      const mainNav = document.querySelector('.main-nav');
+      const links = mainNav.querySelectorAll('a');
 
       const navLinckHandleClick = () => {
-        navMain.classList.add("main-nav--closed");
-        navMain.classList.remove("main-nav--opened");
-      };
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
 
-      links.forEach((link) => {
-        link.addEventListener("click", navLinckHandleClick);
-      });
-    };
+      links.forEach(link => {
+        link.addEventListener('click', navLinckHandleClick)
+      })
+    }
 
     initJS();
     closeOpen();
